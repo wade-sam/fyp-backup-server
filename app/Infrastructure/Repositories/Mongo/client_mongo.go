923876d7dba26128ec3265bde127e548def3c8d4
Sam <sam@Sams-MBP-2.localdomain>
2021-02-26 00:24:09 +0000

    started mongo repository. Nearly finished with client, just need to unit test

--- app/Infrastructure/Repositories/Mongo/client_mongo.go
+++ app/Infrastructure/Repositories/Mongo/client_mongo.go
@@ -2,10 +2,13 @@ package Mongo
 
 import (
 	"context"
+	"log"
 	"time"
 
 	"github.com/pkg/errors"
+	"github.com/wade-sam/fyp-backup-server/Infrastructure/Repositories/Mongo/objects"
 	"github.com/wade-sam/fyp-backup-server/entity"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -30,11 +33,84 @@ func (c *ClientMongo) Create(client *entity.Client) (*entity.Client, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
 	defer cancel()
 	collection := c.db.Database(c.database).Collection(c.collection)
-	b_client := client
-	insertResult, err := collection.InsertOne(ctx, b_client)
+	b_client := &objects.Client{
+		Clientname:    client.Clientname,
+		Policies:      client.Policies,
+		Directorytree: client.Directorytree,
+		Ignorepath:    client.Ignorepath,
+		Backups:       client.Backups,
+	}
+	_, err := collection.InsertOne(ctx, b_client)
 
 	if err != nil {
 		return nil, errors.Wrap(err, "repository.CreateClient")
 	}
 	return client, nil
+}
+
+func (c *ClientMongo) Update(client *entity.Client) error {
+	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
+	defer cancel()
+	collection := c.db.Database(c.database).Collection("clients_collection")
+
+	b_client := &objects.Client{
+		Clientname:    client.Clientname,
+		Policies:      client.Policies,
+		Directorytree: client.Directorytree,
+		Ignorepath:    client.Ignorepath,
+		Backups:       client.Backups,
+	}
+
+	result, err := collection.UpdateMany(
+		ctx,
+		bson.M{"clientname": b_client.Clientname},
+		bson.M{"$set": b_client},
+	)
+	if err != nil {
+		return errors.Wrap(err, "repository.Policy.Update")
+	}
+	log.Println(result)
+	return nil
+}
+
+func (c *ClientMongo) Delete(name string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
+	defer cancel()
+	collection := c.db.Database(c.database).Collection("clients_collection")
+	_, err := collection.DeleteOne(ctx, bson.M{"clientname": name})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *ClientMongo) Get(name string) (*entity.Client, error) {
+	b_client := objects.Client{}
+	client := &entity.Client{}
+
+	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
+	defer cancel()
+	collection := c.db.Database(c.database).Collection("clients_collection")
+	filter := bson.M{"clientname": name}
+	err := collection.FindOne(ctx, filter).Decode(&b_client)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, errors.Wrap(err, "repository.FindClient")
+		}
+		return nil, errors.Wrap(err, "Repository.Client.Find")
+	}
+	client.Clientname = b_client.Clientname
+	client.Policies = b_client.Policies
+	client.Directorytree = b_client.Directorytree
+	client.Ignorepath = b_client.Ignorepath
+	client.Backups = b_client.Backups
+	return client, nil
+}
+
+func (c *ClientMongo) List() ([]*entity.Client, error) {
+	clients := []*entity.Client{}
+	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
+	defer cancel()
+	collection := c.db.Database(c.database).Collection("clients_collection")
+	cursor, err := collection.Find(ctx)
 }
--- app/go.mod
+++ app/go.mod
@@ -2,4 +2,9 @@ module github.com/wade-sam/fyp-backup-server
 
 go 1.15
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/streadway/amqp v1.0.0
+	github.com/stretchr/testify v1.7.0
+	github.com/vektra/mockery v1.1.2 // indirect
+	go.mongodb.org/mongo-driver v1.4.6
+)
